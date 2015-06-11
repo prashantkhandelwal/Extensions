407212eb57190cdc37f2b35de5ee270a613fd035
Prashant Khandelwal <prashant@midnightprogrammer.net>
2015-06-11 20:20:37 +0530

    Added CollectionExtensions.cs
    Extension methods for Collections.

---
+++ Extensions.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extensions.Collections;
+
+namespace Extensions.Tests
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        [TestMethod]
+        public void RemoveDuplicates_Test()
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add("val 1");
+            arr.Add("val 1");
+            arr.Add("val 2");
+            arr.Add("val 3");
+            arr.Add("val 4");
+            arr.Add("val 4");
+            arr.Add("val 5");
+
+            ArrayList _arr = new ArrayList();
+            _arr.Add("val 1");
+            _arr.Add("val 2");
+            _arr.Add("val 3");
+            _arr.Add("val 4");
+            _arr.Add("val 5");
+
+            Assert.AreEqual(_arr.Count, arr.RemoveDuplicates().Count);
+        }
+    }
+}

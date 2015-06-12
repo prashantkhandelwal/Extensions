bd6fce77adbb0e572193a1ebad8441ccd6adf371
Prashant Khandelwal <prashant@midnightprogrammer.net>
2015-06-12 19:57:59 +0530

    Custom data types
    FixedSizeQueue

---
+++ Extensions.Tests/CustomDataTypesTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Extensions.CustomDataTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extensions.Tests
+{
+    [TestClass]
+    public class CustomDataTypesTest
+    {
+        [TestMethod]
+        public void FixedSizedQueue_Test()
+        {
+            FixedSizeQueue<string> q = new FixedSizeQueue<string>(5);
+            q.Enqueue("1");
+            q.Enqueue("2");
+            q.Enqueue("3");
+            q.Enqueue("4");
+            q.Enqueue("5");
+            q.Enqueue("6");
+            q.Enqueue("7");
+            q.Enqueue("8");
+
+            Assert.AreEqual(5, q.Count);
+        }
+    }
+}
---
+++ Extensions/CustomDataTypes/FixedSizeQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions.CustomDataTypes
+{
+
+    /// <summary>
+    /// Create a Queue with a specified length
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>As used in https://github.com/ShareX/ShareX</remarks>
+    public class FixedSizeQueue<T> : Queue<T>
+    {
+        public int Size { get; set; }
+
+        public FixedSizeQueue(int size)
+        {
+            Size = size;
+        }
+
+        public new void Enqueue(T obj)
+        {
+            base.Enqueue(obj);
+            while (Count > Size) Dequeue();
+        }
+    }
+}
